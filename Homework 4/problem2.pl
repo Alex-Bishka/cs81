d9ff360d058330a8f28e0dafbb950fd10ca84065
Alex-Bishka <abishka@g.hmc.edu>
2020-09-24 05:22:11 -0500

    passes test cases for p2

--- Homework 4/problem2.pl
+++ Homework 4/problem2.pl
@@ -11,5 +11,29 @@ path(X, X, _, [X], K) :- K >= 0.
 
 %%%%%%%%%%%%%
 %% YOUR TURN!
+% Description:
+%   our predicate is true iff Path is a list of vertices comprising a path
+%   from vertex start to vertex end in the given weighted directed Graph
+%   such that the sum of the costs of the edges in the path does not exceed 
+%   the given budget
+%
+% Notes:
+%   a graph is represented as a list of triples where a triple [x, y, z]
+%   denotes an edge from x to y with a weight of z
+%   (x = start; y = end; z is cost - aka weight)
+%   z is always postive.
+%   cycles can exist!
+%
+% What our predicate looks like:  
+%   path(start_vertex, end_vertex, graph, path, budget)
+%
+% To run:
+% [problem2].
+% dynamic graph/1.
+% assert(graph(  [ [a,b,1], [b,c,2], [c,b,1], [b,a,5], [c,d,2],[b,w,2], [w,x,3], [x,z,2], [b,z,5] ] )).
+% graph(G), path(a, z, G, P, 5).
+% graph(G), path(a, z, G, P, 6).
+% graph(G), path(a, z, G, P, 8).
+% graph(G), path(a, z, G, P, 10).
 %%%%%%%%%%%%%
-path(X, Y, Graph, [X | Rest], Budget) :- fail. % <--- write your code here, replacing 'fail.'
+path(X, Y, Graph, [X | Rest], Budget) :- member([X, NewStart, Weight], Graph), NewWeight is Budget - Weight, \+ Weight > Budget, path(NewStart, Y, Graph, Rest, NewWeight).
